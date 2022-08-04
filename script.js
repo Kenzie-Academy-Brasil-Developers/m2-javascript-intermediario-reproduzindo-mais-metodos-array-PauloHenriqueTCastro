d07db305df6355292b4a6d7d82034996c331cc7d
PauloH <tilapttavares@gmail.com>
2022-08-04 14:49:47 -0300

    primeira modificação

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 //Método forEach --------
-const arrayForEach = [10, 20, 30, 44, 55]; // array para iteração do método
-
-//array em que nossa callback vai colocar os números multiplicados
+const arrayForEach = [10, 20, 30, 44, 55];
 const arrayVazio = [];
-//nossa callback
 function callbackForEach(element) {
   return arrayVazio.push(element * 2);
 }
-
 function forEach(array, callback) {
-  //sua lógica aqui
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i])
+  }
 }
+//console.log(forEach(arrayForEach, callbackForEach));
+//console.log(arrayVazio);
 
-// console.log(forEach(arrayForEach, callbackForEach));
-// console.log(arrayVazio);
-
-//Método Find --------
-
+//Método Find
 const arrayFind = [12, true, "alfafa", 4, 5]; // array para iteração do método
 
-//nossa primeira callback testa se o elemento é maior que 2 e menor que 12
 function callbackFind(element) {
   if (element > 2 && element < 12) {
     return true;
   }
 }
 
-//nossa segunda callback testa se existe algum elemento maior que 15
 function callbackFindTwo(element) {
   if (element > 15) {
     return true;
@@ -34,44 +28,60 @@ function callbackFindTwo(element) {
 }
 
 function find(array, callback) {
-  //sua lógica aqui
+  for (let i = 0; i < array.length; i++) {
+    let elementoArr = array[i]
+    let resultado = callback(elementoArr)
+    if (resultado === true) {
+      return elementoArr
+    }
+  }
 }
-// console.log(find(arrayFind, callbackFind));
-// console.log(find(arrayFind, callbackFindTwo));
+//console.log(find(arrayFind, callbackFind));
+//console.log(find(arrayFind, callbackFindTwo));
 
 //Método IndexOf --------
 
-const arrayFindIndex = [595, 206, "apollo", false, null, undefined, 8]; // array para iteração do método
+const arrayFindIndex = [595, 206, "apollo", false, null, undefined, 8];
 
-//primeira callback que procura algum elemento que seja uma string
 function callbackFindIndex(element) {
   return typeof element === "string" ? true : false;
 }
 
-//segunda callback que testa se existe algum elemento igual a 3 no array
 function callbackFindIndexTwo(element) {
   return element == 3 ? true : false;
 }
 
 function FindIndex(array, callback) {
-  //sua lógica aqui
+  for (let i = 0; i < array.length; i++) {
+    let elementArr = array[i]
+    let resultado = callback(elementArr)
+    if (resultado === true) {
+      return i
+    }
+  } return -1
 }
-// console.log(FindIndex(arrayFindIndex, callbackFindIndex));
-// console.log(FindIndex(arrayFindIndex, callbackFindIndexTwo));
+//console.log(FindIndex(arrayFindIndex, callbackFindIndex));
+//console.log(FindIndex(arrayFindIndex, callbackFindIndexTwo));
 
 //Método Includes --------
 
-const arrayIncludes = [53, 2, "duck", NaN, 456, 7]; // array para iteração do método
+const arrayIncludes = [53, 2, "duck", NaN, 456, 7];
 
 function includes(array, searchElement) {
-  //sua lógica aqui
+  for (let i = 0; i < array.length; i++) {
+    let elementArr = array[i]
+    let elementoPro = searchElement
+    if (elementArr === elementoPro) {
+      return true
+    }
+  } return false
 }
 
-// console.log(includes(arrayIncludes, "duck"));
-// console.log(includes(arrayIncludes, 500));
+//console.log(includes(arrayIncludes, "duck"));
+//console.log(includes(arrayIncludes, 500));
 
 //Método some --------
-const arrySome = [12, 82, 563, 414, null]; // array para iteração do método
+const arrySome = [12, 82, 563, 414, null];
 const callbackSome = (element) => {
   return element > 82 ? true : false;
 };
@@ -83,20 +93,29 @@ const callbackSome2 = (element) => {
 };
 
 function some(array, callback) {
-  //sua lógica aqui
+  for (let i = 0; i < array.length; i++) {
+    let elementArr = array[i]
+    let resultado = callback(array[i])
+    if (resultado === true) {
+      return true
+    }
+  } return false
 }
 
-// console.log(some(arrySome, callbackSome));
-// console.log(some(arrySome, callbackSome2));
+//console.log(some(arrySome, callbackSome));
+//console.log(some(arrySome, callbackSome2));
 
 //Método Join --------
 
-const arrayJoin = ["Fogo", "no", "Parquinho"]; // array para iteração do método
+const arrayJoin = ["Fogo", "no", "Parquinho"];
 
 function join(array, value) {
-  //sua lógica aqui
-}
+  let resultado = ''
+  for (let i = 0; i < array.length; i++) {
+    resultado += array[i]
+    resultado += value
 
-// console.log(join(arrayJoin, " "));
-// console.log(join(arrayJoin, ""));
-// console.log(join(arrayJoin, "-"));
+  } return resultado
+}
+console.log(join(arrayJoin, " "));
+console.log(join(arrayJoin, ""));
